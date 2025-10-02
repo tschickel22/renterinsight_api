23500668fdff205582de4dbb78c30b9d604996b1
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-10-02 13:45:55 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::API
   before_action :authenticate
-
+  
   private
-
+  
   def authenticate
     @current_company_id = (request.headers['X-Company-Id'] || 1).to_i
   end
-
+  
   def current_company_id
     @current_company_id
+  end
+  
+  def current_user
+    # Temporary implementation until you add user authentication
+    # Returns a simple object with id and company association
+    @current_user ||= OpenStruct.new(id: 1, company_id: current_company_id)
   end
 end
