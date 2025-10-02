b9f582233a7d35007e369940c53ce882c32182ac
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-10-02 17:29:46 -0600

    Update lead.rb

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,3 +1,6 @@
 class Lead < ApplicationRecord
   belongs_to :source, optional: true  # allow creating leads while testing
+
+  # NEW: link leads -> activities
+  has_many :activities, dependent: :destroy
 end
