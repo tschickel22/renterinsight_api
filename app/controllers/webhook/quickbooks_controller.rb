5c69739b3c444576e31d69b936d73bd0096589bf
Your Name <you@example.com>
2025-12-27 12:56:38 -0700

    Fix QuickBooks webhook controller: add raise:false to skip_before_action
    Prevents deployment error when verify_authenticity_token callback doesn't exist

--- app/controllers/webhook/quickbooks_controller.rb
+++ app/controllers/webhook/quickbooks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Webhook::QuickbooksController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, raise: false
   skip_before_action :authenticate
   
   def notifications
