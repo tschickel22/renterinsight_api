3203f25e6227a23f710343859479d2f1dd4d8709
Tom S. <toms.@Toms-MacBook-Air.local>
2026-05-27 17:08:38 -0600

    diag: log Solid Queue plugin load decision

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,11 @@ port ENV.fetch("PORT", 3000)
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+puts "[puma.rb] SOLID_QUEUE_IN_PUMA=#{ENV['SOLID_QUEUE_IN_PUMA'].inspect}"
+if ENV["SOLID_QUEUE_IN_PUMA"]
+  puts "[puma.rb] Registering solid_queue plugin"
+  plugin :solid_queue
+end
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
